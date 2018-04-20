88f8a9d23296b3d15a3a3d0a4c21a713344513e8
Javed Wright <jdwright870@gmail.com>
2018-04-20 21:37:11 +0000

    finished home added routes to header

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -10,13 +10,33 @@ Vue.component('app-header', {
     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
-          </li>
         </ul>
       </div>
+      
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/explore">Explore</router-link>
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" to="#">My Profile</router-link>
+            </li>
+            <li v-if="token" class="nav-item active">
+              <router-link class="nav-link" to="/logout">Logout</router-link>
+            </li>
+            <li v-else class="nav-item">
+              <router-link class="nav-link active" to="/login">Login</router-link>
+            </li>
+        </ul>
     </nav>
-    `
+    `,
+    data: function(){
+        return {
+            token: localStorage.token
+        }
+    }
 });
 
 Vue.component('app-footer', {
@@ -31,12 +51,22 @@ Vue.component('app-footer', {
 
 const Home = Vue.component('home', {
    template: `
-    <div class="row container-center">
-        <div class="col-md-4 container-child">
+    <div class="row landing-container">
+        <div class="col-md-4 landing-container-child" style="margin-left: 15%;">
             <img src="/static/images/landing.jpg" id="landing-img"/>
         </div>
-        <div class="col-md-4 container-child">
-          <h1>insert form here</h1>  
+        <div class="col-md-4  landing-container-child float-clear">
+          <div class="card" style="width: 28rem; height: 23rem; box-shadow: 2px 2px 10px grey;">
+            <img class="card-img-top" src="static/images/photogramLogo.png" alt="Card image cap" style="width: 60%; margin: 0 auto; padding-top: 20px;">
+            <div class="card-body" style="padding-top: 0px;">
+              <hr>
+              <p class="card-text">Share photos of your favourite moments with friends, family and the world.</p>
+              <div style="margin-top: 20%;">
+                  <button class="btn btn-success col-md-5">Register</button>
+                  <button class="btn btn-primary col-md-5">Login</button>
+              </div>
+            </div>
+          </div>  
         </div>
     </div>
    `,
@@ -49,7 +79,12 @@ const Home = Vue.component('home', {
 // Define Routes
 const router = new VueRouter({
     routes: [
-        { path: "/", component: Home }
+        { path: "/", component: Home },
+        { path: "/register", component: Register},
+        { path: "/login",},
+        {path: "/logout",},
+        {path: "explore"},
+        
     ]
 });
 
