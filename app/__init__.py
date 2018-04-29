554181583ace9e8d3072eee9de8595b5ac5f38ce
Javed Wright <jdwright870@gmail.com>
2018-04-29 19:50:49 +0000

    updated heroku postgresql url

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ csrf = CSRFProtect(app)
 
 bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = "this is a super secure key"  # you should make this more random and unique
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ajlnzyeyfdyqmr:d49e01c9e341b929299dddbf28de69c5129cb68c8a4a598be5f4a3faf347e77b@ec2-174-129-41-64.compute-1.amazonaws.com:5432/d9nnhbajbimvol'"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lmpxecwaaxsise:47da5b91c1f6c6373bc721a12d89333179589602dd2dd99af4b0826bfcee2aaa@ec2-23-23-142-5.compute-1.amazonaws.com:5432/d6p863j1eo19b8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
 
 UPLOAD_FOLDER ='./app/static/uploads'
